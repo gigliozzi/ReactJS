f877748e3a0da77fdc037e1d4a1dd599b6e86d03
William Gigliozzi <gigliozzi@gmail.com>
2021-05-12 18:32:50 -0300

    Add Bootstrap, add react-router-dom

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,22 @@
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container py-5">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="btn btn-primary bt-large mt-4" to="/dashboard" target="_blank">acessar dashboard</Link>
+                </div>
+                
+            </div>
+        </>
+    );
+}
+
+export default Home;
